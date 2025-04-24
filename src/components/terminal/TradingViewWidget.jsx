a5cf4c419b2fcaacac539e1dbc1f181e279baa34
ArtCenter1 <artcenter1@gmail.com>
2025-04-25 00:12:11 +0800

    tradingView Drawing tool added

--- src/components/terminal/TradingViewWidget.jsx
+++ src/components/terminal/TradingViewWidget.jsx
@@ -6,15 +6,24 @@ function TradingViewWidget({ selectedPair, timeframe }) {
   // Map timeframe to TradingView interval
   const getInterval = () => {
     switch (timeframe) {
-      case '1': return '1';
-      case '5': return '5';
-      case '15': return '15';
-      case '30': return '30';
-      case '60': return '60';
-      case '240': return '240';
-      case 'D': return 'D';
-      case 'W': return 'W';
-      default: return 'D'; // Default to daily
+      case '1':
+        return '1';
+      case '5':
+        return '5';
+      case '15':
+        return '15';
+      case '30':
+        return '30';
+      case '60':
+        return '60';
+      case '240':
+        return '240';
+      case 'D':
+        return 'D';
+      case 'W':
+        return 'W';
+      default:
+        return 'D'; // Default to daily
     }
   };
 
@@ -51,15 +60,16 @@ function TradingViewWidget({ selectedPair, timeframe }) {
     }
 
     // Create and add the script
-    const script = document.createElement("script");
-    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-    script.type = "text/javascript";
+    const script = document.createElement('script');
+    script.src =
+      'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
+    script.type = 'text/javascript';
     script.async = true;
-    
+
     // Get the symbol and interval
     const symbol = getSymbol();
     const interval = getInterval();
-    
+
     // Configure the widget
     script.innerHTML = `
       {
@@ -73,14 +83,31 @@ function TradingViewWidget({ selectedPair, timeframe }) {
         "toolbar_bg": "#131722",
         "enable_publishing": false,
         "hide_top_toolbar": false,
+        "hide_side_toolbar": false,
         "allow_symbol_change": true,
         "save_image": false,
-        "support_host": "https://www.tradingview.com"
+        "support_host": "https://www.tradingview.com",
+        "drawings_access": { "type": "all" },
+        "show_popup_button": true,
+        "popup_width": "1000",
+        "popup_height": "650",
+        "studies": ["Volume@tv-basicstudies"],
+        "enabled_features": [
+          "side_toolbar_in_fullscreen_mode",
+          "drawing_templates"
+        ],
+        "disabled_features": [
+          "header_symbol_search",
+          "header_compare",
+          "header_undo_redo",
+          "header_screenshot",
+          "use_localstorage_for_settings"
+        ]
       }`;
-    
+
     // Add the script to the container
     container.current.appendChild(script);
-    
+
     // Cleanup function
     return () => {
       if (container.current) {
@@ -93,9 +120,24 @@ function TradingViewWidget({ selectedPair, timeframe }) {
   }, [selectedPair, timeframe]); // Re-run when selectedPair or timeframe changes
 
   return (
-    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
-      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
-      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a></div>
+    <div
+      className="tradingview-widget-container"
+      ref={container}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <div
+        className="tradingview-widget-container__widget"
+        style={{ height: 'calc(100% - 32px)', width: '100%' }}
+      ></div>
+      <div className="tradingview-widget-copyright">
+        <a
+          href="https://www.tradingview.com/"
+          rel="noopener nofollow"
+          target="_blank"
+        >
+          <span className="blue-text">Track all markets on TradingView</span>
+        </a>
+      </div>
     </div>
   );
 }
